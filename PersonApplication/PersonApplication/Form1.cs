970c5a593228e8166368e0dd17793e18599b2943
ZahidHasan4mJU <responsible.zahid@yahoo.com>
2014-09-26 22:18:32 +0600

    uploaded

---
+++ PersonApplication/PersonApplication/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonApplication
+{
+    public partial class PersonAppsForm : Form
+    {
+        public PersonAppsForm()
+        {
+            InitializeComponent();
+        }
+        Persons p = new Persons();
+        private void fullButton_Click(object sender, EventArgs e)
+        {
+            p.fName = firstTextBox.Text;
+            p.mName = middleTextBox.Text;
+            p.lName = lastTextBox.Text;
+
+            fullTextBox.Text = p.getFullName();
+        }
+
+        private void reverseButton_Click(object sender, EventArgs e)
+        {
+            reverseTextBox.Text = p.getReverseName();
+        }
+    }
+}
---
+++ PersonApplication/PersonApplication/Persons.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonApplication
+{
+    class Persons
+    {
+        public string fName;
+        public string mName;
+        public string lName;
+
+        public string getFullName()
+        {
+            return fName + "  " + mName + "  " + lName;
+        }
+
+        public string getReverseName()
+        {
+            string fullname = getFullName();
+            char[] array = fullname.ToCharArray();
+            string reverseName = "";
+            for (int i = array.Length - 1; i > -1; i--)
+            {
+                reverseName += array[i];
+                
+            }
+            return reverseName;
+
+        }
+        
+    }
+}
